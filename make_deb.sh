5de1344c02935ea0d44b0d810e4d4bda083ea20e
bogdan3d <bogdan3d@gmail.com>
2025-02-07 11:48:02 -0500

    77916 Additions to support creating deb package.

---
+++ make_deb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp debian/changelog debian/changelog.save
+
+VERSION=6.2.3-rev1
+
+# update the changelog file with the package version
+dch -v $VERSION -m auto-build
+
+dpkg-buildpackage -b -us -uc
+
+mv debian/changelog.save debian/changelog
